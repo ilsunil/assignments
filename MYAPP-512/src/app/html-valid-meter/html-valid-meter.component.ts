7f51a2d750aaf49209fc704aff0b543693f22274
Sunil B <bsunilmon@gmail.com>
2019-05-12 13:12:10 +0530

    NEW

---
+++ MYAPP-512/src/app/html-valid-meter/html-valid-meter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html-valid-meter',
+  templateUrl: './html-valid-meter.component.html',
+  styleUrls: ['./html-valid-meter.component.css']
+})
+export class HtmlValidMeterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
